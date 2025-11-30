aea33a7212be0deb8afa4f4956f5222b9011aae3
mithibe irungu <mithibeirungu@gmail.com>
2025-11-30 16:56:04 +0300

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,22 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+
+    'allowed_origins' => [
+        'https://lami-henna.vercel.app',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+
+    'exposed_headers' => false,
+
     'max_age' => 0,
-    'supports_credentials' => false,
+
+    'supports_credentials' => true,
 ];
+
